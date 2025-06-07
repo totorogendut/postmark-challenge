a0198db92230feb7c7c682593009f077c3cc476e
ProgNovel <prognovel@gmail.com>
2025-06-08 04:00:34 +0800

    fix cloudflare page upload

--- src/lib/server/ai/index.ts
+++ src/lib/server/ai/index.ts
@@ -1,30 +1,30 @@
-import { OPENAI_API_KEY, GEMINI_API_KEY, ANTHROPIC_API_KEY } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { createAnthropic } from "@ai-sdk/anthropic";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 import type { LanguageModelV1 } from "ai";
 
 export const anthropic = createAnthropic({
-	apiKey: ANTHROPIC_API_KEY,
+	apiKey: env.ANTHROPIC_API_KEY,
 });
 
 export const openai = createOpenAI({
-	apiKey: OPENAI_API_KEY,
+	apiKey: env.OPENAI_API_KEY,
 });
 
 export const gemini = createGoogleGenerativeAI({
-	apiKey: GEMINI_API_KEY,
+	apiKey: env.GEMINI_API_KEY,
 });
 
 export const LLMs = {
 	structuredOutputs: [
-		!!OPENAI_API_KEY && openai("gpt-4.1", { structuredOutputs: true }),
-		!!GEMINI_API_KEY && gemini("gemini-2.5-flash-preview-04-17", { structuredOutputs: true }),
+		!!env.OPENAI_API_KEY && openai("gpt-4.1", { structuredOutputs: true }),
+		!!env.GEMINI_API_KEY && gemini("gemini-2.5-flash-preview-04-17", { structuredOutputs: true }),
 	].filter((llm) => !!llm),
 	text: [
-		!!ANTHROPIC_API_KEY && anthropic("claude-4-opus-20250514"),
-		!!OPENAI_API_KEY && openai("gpt-4.1"),
-		!!GEMINI_API_KEY && gemini("gemini-2.5-flash-preview-04-17"),
+		!!env.ANTHROPIC_API_KEY && anthropic("claude-4-opus-20250514"),
+		!!env.OPENAI_API_KEY && openai("gpt-4.1"),
+		!!env.GEMINI_API_KEY && gemini("gemini-2.5-flash-preview-04-17"),
 	].filter((llm) => !!llm),
 };
 
