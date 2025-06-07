cdd4647bc511d6b777d58e263769e37bd2addf1a
ProgNovel <prognovel@gmail.com>
2025-06-08 04:11:25 +0800

    pages again...

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	build: {},
+	ssr: {
+		external: ["@node-rs/argon2-wasm32-wasi"],
+	},
 	test: {},
 });
